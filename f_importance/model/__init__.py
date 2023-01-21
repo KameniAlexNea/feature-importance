7290688ea48ce2747d539cc8ba66f10c607946a0
Kamenialexnea <Kamenialexnea@gmail.com>
2023-01-21 21:17:26 +0100

    add models class splitted into reg and class

--- f_importance/model/__init__.py
+++ f_importance/model/__init__.py
@@ -2,3 +2,16 @@
     In this package, we manipulate everything
     about models
 """
+
+from lightgbm import LGBMClassifier, LGBMRegressor
+from xgboost import XGBClassifier, XGBRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+
+CLASSIFIERS = dict(
+    XGBClassifier=XGBClassifier, LGBMClassifier=LGBMClassifier, RandomForestClassifier=RandomForestClassifier, GradientBoostingClassifier=GradientBoostingClassifier
+)
+
+REGRESSORS = dict(
+    XGBRegressor=XGBRegressor, LGBMRegressor=LGBMRegressor, RandomForestRegressor=RandomForestRegressor, GradientBoostingRegressor=GradientBoostingRegressor
+)
